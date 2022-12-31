875ecb5ee9b65fc12f54b1475d32762ce10bc90c
londhe-7083 <shubhamlondhe1583@gmail.com>
2022-12-31 14:35:59 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,79 +1,30 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import logo from "./views/immg/basketball.png";
+import React,{useState} from 'react'
+import "./App.css"
 
 function App() {
-  const[scoreA, setScoreA] = useState(0);
-  const[scoreB, setScoreB] = useState(0);
 
-  useEffect(()=>{  
-  const a =localStorage.getItem("scoreA");
-  const b =localStorage.getItem("scoreB");
-
-  if((a)){
-    setScoreA(a);
-  }
-
-  if((b)){
-    setScoreB(b);
-  }
-  }, [])
-
-useEffect(()=>{ 
-  if(scoreA > 0)
-  {
-    localStorage.setItem("scoreA", scoreA);
-  }
-  
-  if(scoreB > 0)
-  {
-    localStorage.setItem("scoreB", scoreB);
-  }
-  },[scoreA,scoreB])
-
-  function resetScore(){
-    setScoreA(0);
-    setScoreB(0);
-    localStorage.clear();
-  }
-
-  
+  const [emoji, setEmoji] = useState("🏆")
   return (
-    <>
-    <div className='score-keeper-container' >
-            <img src={logo} className="img-basket" />
-            <h2 className='score-keeper-heading'> Basketball-Score-keeper </h2>
+    <div className='container'>
+      <div className="app-title-container">
+        <h3 className='app-title'>😍emoji-kit</h3>
+      </div>
 
-            <div className='score-card-container'>
+      <div className='main-emoji-container'>
+        <div className='emoji'>
+          {emoji}
+        </div>
+      </div>
 
-              <div className="score-card">
-                <h3 className='score-card-heading'>Team A</h3>
-                <h6 className='score-card-count'> {scoreA} </h6>
-                <div className='button-container'>
-                  <button className="score-button" onClick={()=>{setScoreA(scoreA+1)}}>+</button>
-                  <button className="score-button" onClick={()=>{setScoreA(scoreA-1)}}>-</button>
-                </div>
-              </div>
-
-              <div className="score-card">
-                <h3 className='score-card-heading'>Team B</h3>
-                <h6 className='score-card-count'> {scoreB} </h6>
-                <div className='button-container'>
-                  <button className="score-button" onClick={()=>{setScoreB(scoreB+1)}}>+</button>
-                  <button className="score-button" onClick={()=>{setScoreB(scoreB-1)}}>-</button>
-                </div>
-              </div>
-
-
-            </div>
-
-           <div>
-            <button className="reset-button" onClick={resetScore}> Reset  </button>
-           </div>
+      <div className='emoji-container'>
+        <div className='emoji-item' onClick={()=>{setEmoji('🍕')}}>🍕</div>
+        <div className='emoji-item' onClick={()=>{setEmoji('🍕')}}>✌️</div>
+        <div className='emoji-item' onClick={()=>{setEmoji('🍕')}}>💕</div>
+        <div className='emoji-item' onClick={()=>{setEmoji('🍕')}}>🙂</div>
+        <div className='emoji-item' onClick={()=>{setEmoji('🍕')}}>😄</div>
+      </div>
     </div>
-    </>
-
-  );
+  )
 }
 
-export default App;
+export default App
